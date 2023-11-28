2b68c64268518e7fca01fd585ba96d24dfae23aa
Gabriel Khot Garang Pawuoi <141131685+KhotKeys@users.noreply.github.com>
2023-11-28 11:15:10 -0800

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,3 @@
+print("Welcome to BodyMath: A smart BMI calculator")
+print("This application is designed to assess Body-Mass-Index, a common metrix used for gauging"
+         " body weight in relation to height")
